f174f6a96aa4de81793f02268313586e841f6ad1
Gleb BUSHUKIN <gleb.bushukin@gmail.com>
2024-12-17 14:58:50 +0100

    deps init

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: [
+    // 'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'prettier'
+    // Make sure "prettier" is the last element in this list.
+  ],
+}
